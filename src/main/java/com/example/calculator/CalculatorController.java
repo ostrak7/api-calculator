b3c9bf2fc130a61fcdcc27e3a1f3887b281e6554
Azur <azur.ostrakovic@gmail.com>
2023-06-08 13:08:22 +0200

    add CalculatorController and sum function

---
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -0,0 +1,22 @@
+package com.example.calculator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CalculatorController {
+
+    /*@GetMapping ("/")
+    public String index(){
+        return  "Hello";
+    }*/
+    @RequestMapping("/sum")
+    public int sum(@RequestParam int a,@RequestParam int b){
+        return a+b;
+    }
+
+}
